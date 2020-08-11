4fbdf3fff0522b4ab170de23ededd1738e4cadf2
Adam Ye <adamye48@gmail.com>
2020-08-11 22:42:39 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ def update_array_at_with(array, index, value)
   array[index] = value
   end
   
-  def update_array_at_with(board, "O", "X")
+  def update_array_at_with(board)
     board[0] = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
 
 end
